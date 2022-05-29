cf9852ccb26c566e7b3920bca36533129fd321f5
avdome <andreivlad.dome@gmail.com>
2022-05-29 07:07:53 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, Image } from 'react-native';
+import Constants from 'expo-constants';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+// You can import from local files
+
+// or any pure javascript modules available in npm
+
 
 export default function App() {
+  const handlePress = () => console.log("Text Clicked")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      < Image 
+      fadeDuration = {1000}
+      source={require("./app/assets/logo.png")} / >
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    //paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'lightgrey',
     justifyContent: 'center',
+    alignItems : "center",
+    //padding: 8,
   },
+  //paragraph: {
+   // margin: 24,
+   // fontSize: 18,
+   // fontWeight: 'bold',
+    //textAlign: 'center',
+ // },
 });
